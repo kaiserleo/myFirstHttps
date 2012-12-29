f8c0da867867d49e591420293157383d0f1ca014
Leonardo de la Cerda <leobiwankenobi@hotmail.com>
2012-12-29 13:09:50 -0600

    First commit

---
+++ main.rb
@@ -0,0 +1,31 @@
+require 'rubygems'
+require 'sinatra/base'
+require 'webrick'
+require 'webrick/https'
+require 'openssl'
+require 'rack'
+require 'rack/request'
+require 'rack-ssl'
+
+use Rack::SSL
+
+class MyServer  < Sinatra::Base
+    get '/' do
+      "Hellow, world!"
+    end
+end
+
+CERT_PATH = '/System/Library/OpenSSL/certs/'
+
+webrick_options = {
+        :Port               => 80,
+        :Logger             => WEBrick::Log::new($stderr, WEBrick::Log::DEBUG),
+        :DocumentRoot       => "/ruby/htdocs",
+        :SSLEnable          => true,
+        :SSLVerifyClient    => OpenSSL::SSL::VERIFY_NONE,
+        :SSLCertificate     => OpenSSL::X509::Certificate.new(  File.open(File.join(CERT_PATH, "kueski.test.crt")).read),
+        :SSLPrivateKey      => OpenSSL::PKey::RSA.new(          File.open(File.join(CERT_PATH, "kueski.test.key")).read),
+        :SSLCertName        => [ [ "CN",WEBrick::Utils::getservername ] ],
+        :app                => MyServer
+}
+Rack::Server.start webrick_options
